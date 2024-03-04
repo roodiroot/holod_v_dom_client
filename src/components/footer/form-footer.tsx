414d8da91b6dd0257204f74fe5dfcb43de1d0ea9
RoodiRoot <borisov130490@gmail.com>
2024-03-04 16:07:18 +0300

    add logo

--- src/components/footer/form-footer.tsx
+++ src/components/footer/form-footer.tsx
@@ -2,14 +2,21 @@ import PromptForm from "@/components/action-forms/prompt-form";
 
 const FormFooter = () => {
   return (
-    <div className='mt-12 md:col-span-8 md:col-start-3 md:row-start-2 md:mt-0 lg:col-span-4 lg:col-start-9 lg:row-start-1'>
-      <h3 className='text-sm font-medium text-gray-900'>
+    <div className="mt-12 md:col-span-8 md:col-start-3 md:row-start-2 md:mt-0 lg:col-span-4 lg:col-start-9 lg:row-start-1">
+      <h3 className="text-sm font-medium text-gray-900">
         Форма обратной связи
       </h3>
-      <p className='mt-6 text-sm'>
+      <p className="mt-6 text-sm">
         Оставьте номер если есть вопрос Мы свяжемся в течении пяти минут.
       </p>
       <PromptForm />
+
+      <div className="mt-8">
+        <a className="text-xs" href="https://matryoshka-studio.ru/">
+          Разработка и дизайн{" "}
+          <span className="text-accent">Веб студия "Матрёшка"</span>
+        </a>
+      </div>
     </div>
   );
 };
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -13,7 +13,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border shadow-sm border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ",
+          "flex h-10 w-full rounded-md border shadow-sm border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ",
           invalid && "border-rose-500 ",
           className
         )}
