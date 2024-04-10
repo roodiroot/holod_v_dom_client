3f92722029eb2e283e1880078ffba4e24ec70e08
RoodiRoot <borisov130490@gmail.com>
2024-04-10 19:30:29 +0300

    fix:1

--- src/components/pages/hero/table-section/table-works.tsx
+++ src/components/pages/hero/table-section/table-works.tsx
@@ -21,17 +21,23 @@ const TableWorks: React.FC<TableWorksProps> = ({
   const { onOpen } = useModal();
   return (
     <div {...props} className={cn("px-4 sm:px6 lg:px-8", className)}>
-      <div className='sm:flex sm:items-center'>
-        <div className='sm:flex-auto'>
-          <h2 className='font-semibold text-gray-900 '>{title}</h2>
-          <p className='mt-2 text-sm text-balance'>{description}</p>
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h2 className="font-semibold text-gray-900 ">{title}</h2>
+          <p className="mt-2 text-sm text-balance">{description}</p>
         </div>
-        <div className='mt-4 sm:ml-16 sm:mt-0 sm:flex-none'>
+        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
           <Button onClick={() => onOpen()}>Заказать установку</Button>
         </div>
       </div>
       {children}
-      {info && <p className='mt-6 text-sm text-balance'>{info}</p>}
+      {info && <p className="mt-6 text-sm text-balance">{info}</p>}
+      <span className="text-sm">
+        Телефон для заказа или консультации{" "}
+        <a href="tel:+79050096888" className="text-accent font-medium">
+          +7 (905) 009 68 88
+        </a>
+      </span>
     </div>
   );
 };
