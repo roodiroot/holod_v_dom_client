ca87f28b51d4890192f82384409aace44b250e4b
RoodiRoot <borisov130490@gmail.com>
2024-09-06 23:19:25 +0300

    fix:2

--- src/app/(root)/price/page.tsx
+++ src/app/(root)/price/page.tsx
@@ -3,6 +3,9 @@ import TableSection from "@/components/pages/hero/table-section/table-section";
 import { cn } from "@/lib/utils";
 import { Metadata } from "next";
 
+import listWorks from "@/utils/price.json";
+import listAddWorks from "@/utils/additionalWorks.json";
+
 export const metadata: Metadata = {
   title: "Цены на услуги",
   description:
@@ -11,10 +14,10 @@ export const metadata: Metadata = {
 
 const PricePage = () => {
   return (
-    <div className='bg-white'>
-      <div className='py-24 sm:py-32'>
-        <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-          <div className='px-4 mx-auto  lg:mx-0 lg:px-8'>
+    <div className="bg-white">
+      <div className="py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="px-4 mx-auto  lg:mx-0 lg:px-8">
             <h1
               className={cn(
                 beautifulFont.className,
@@ -24,7 +27,7 @@ const PricePage = () => {
               Цены на услуги
             </h1>
           </div>
-          <TableSection />
+          <TableSection listAddWorks={listAddWorks} listWorks={listWorks} />
         </div>
       </div>
     </div>
