b56e098415e497bbf5f3592cb624b28228d5c07c
RoodiRoot <borisov130490@gmail.com>
2024-09-07 17:31:55 +0300

    fix:3

--- src/lib/send-message.ts
+++ src/lib/send-message.ts
@@ -1,7 +1,8 @@
 import { toast } from "sonner";
 
 export const sendMessage = async (data: any) => {
-  const d = await fetch(`/api/order?text=${JSON.stringify(`${data} test`)}`);
+  const d = await fetch(`/api/order?text=${JSON.stringify(data)}`);
+  console.log(data);
   if (d.status === 200) {
     toast.success("Ваша заявка принята!", {
       description: "Ожидайте звонка нашего специалиста.",
