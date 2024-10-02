2aab9e5263ce778c5858ef6f6e76c9d42fb31781
RoodiRoot <borisov130490@gmail.com>
2024-10-02 11:14:46 +0300

    add chat

--- src/components/absolute/absolute.tsx
+++ src/components/absolute/absolute.tsx
@@ -4,12 +4,14 @@ import ModalWindow from "./modal-window";
 import SearchWindow from "./search-window";
 import MobilFilters from "./mobil-filters";
 import ImageShow from "./image-show";
+import CallWidget from "./widgets/call-widget";
 
 const Absolute = () => {
   return (
     <>
       <Toaster position="top-right" />
       <MobilMenu />
+      <CallWidget />
       <MobilFilters />
       <ModalWindow />
       <SearchWindow />
